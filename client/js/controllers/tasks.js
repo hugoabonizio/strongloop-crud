d6179fd2c823113b9af097d7a38896980e3da081
hugo <hugo_abonizio@hotmail.com>
2016-06-01 16:49:55 -0300

    Add destroying task feature

--- client/js/controllers/tasks.js
+++ client/js/controllers/tasks.js
@@ -2,12 +2,24 @@ angular
   .module('app')
   .controller('AllTasksController', ['$scope', '$state', 'Task', function ($scope, $state, Task) {
     $scope.tasks = Task.find()
+
     $scope.update = function (task) {
       task.$save()
     }
+
+    $scope.destroy = function (task) {
+      Task
+        .deleteById({ id: task.id })
+        .$promise.then(function () {
+          var index = $scope.tasks.indexOf(task)
+          $scope.tasks.splice(index, 1)
+        })
+    }
   }])
+
   .controller('AddTaskController', ['$scope', '$state', 'Task', function ($scope, $state, Task) {
     $scope.task = {};
+
     $scope.save = function () {
       Task
         .create($scope.task)
